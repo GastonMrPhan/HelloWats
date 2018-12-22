a65bb7f85a7e237a8f4ce86c8315e750a7c19725
Phan Gaston <phangaston@MacBook-Air-de-Phan.local>
2018-12-22 15:27:35 +0100

    maj

--- dashboard/views.py
+++ dashboard/views.py
@@ -27,7 +27,7 @@ def results(request, client_id):
     ###################################
     con = sqlite3.connect('db.sqlite3')
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM dashboard_conso_eur WHERE client_id=%s;",(client_id,))
+    cursor.execute("SELECT * FROM dashboard_conso_eur WHERE client_id = %s;",(client_id,))
     rows = cursor.fetchall()
     print(rows)
     print(type(rows))
