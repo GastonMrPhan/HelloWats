08068ff7f4ee525e069ead8947b2ebbce1d4d1fa
Phan Gaston <phangaston@MacBook-Air-de-Phan.local>
2018-12-23 18:12:53 +0100

    maj

--- dashboard/views.py
+++ dashboard/views.py
@@ -27,17 +27,32 @@ def results(request, client_id):
     # ----> YOUR CODE GOES HERE <---- #
     ###################################
 
-#On commence par se connecter à la base de donnée
+    #On commence par se connecter à la base de donnée
     con = sqlite3.connect('db.sqlite3')
-#On va affecter les conso en euro et en watt aux variables conso_watt et conso_eur
+    #On va affecter les conso en euro et en watt aux variables conso_watt et conso_eur
     cursor = con.cursor()
     cursor.execute("SELECT * FROM dashboard_conso_eur WHERE client_id = ?",(client_id,))
     conso_euro = cursor.fetchall()
-    print(conso_euro)
+    #On va séparer les deux consommations annuelles
+    conso_euro_2016 = conso_euro[0]
+    conso_euro_2016 = list(conso_euro_2016)
+    #On va garder uniquement les valeurs des consommations des mois dans la liste
+    del conso_euro_2016[0]
+    del conso_euro_2016[len(conso_euro_2016)-1]
+    del conso_euro_2016[len(conso_euro_2016)-1]
+    conso_euro_2017 = conso_euro[1]
+    conso_euro_2017 = list(conso_euro_2017)
+    del conso_euro_2017[0]
+    del conso_euro_2017[len(conso_euro_2017)-1]
+    del conso_euro_2017[len(conso_euro_2017)-1]
+    #On va sommer les consommations pour obtenir les dépenses annuelles.
+    annual_costs=[sum(conso_euro_2016),sum(conso_euro_2017)]
+    print(annual_costs)
+
+
     cursor.execute("SELECT * FROM dashboard_conso_watt WHERE client_id = ?",(client_id,))
     conso_watt = cursor.fetchall()
-    conso_euro_2016 = conso_euro[0]
-    conso_euro_2017 = conso_euro[1]
+
 
 
 
