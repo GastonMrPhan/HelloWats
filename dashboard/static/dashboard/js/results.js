feae2d60bc2aca410bdf4c1486279ebcf24fe8d0
Phan Gaston <phangaston@MacBook-Air-de-Phan.local>
2018-12-26 16:34:28 +0100

    final version

--- dashboard/static/dashboard/js/results.js
+++ dashboard/static/dashboard/js/results.js
@@ -1,25 +1,108 @@
 // ----> YOUR CODE HERE <----
 
-new Morris.Bar({
-  // ID of the element in which to draw the chart.
-  element: 'Consommation',
-  // Chart data records -- each entry in this array corresponds to a point on
-  // the chart.
-  data: [
-    { year: '2008', value: 20 },
-    { year: '2009', value: 10 },
-    { year: '2010', value: 5 },
-    { year: '2011', value: 5 },
-    { year: '2012', value: 20 }
-  ],
-  // The name of the data record attribute that contains x-values.
-  xkey: 'year',
-  // A list of names of data record attributes that contain y-values.
-  ykeys: ['value'],
-  // Labels for the ykeys -- will be displayed when you hover over the
-  // chart.
-  labels: ['Value']
-});
-//On n'affiche pas un block
-document.getElementById("chauffage").style.display="none";
 
+//Tableau des dépenses
+//On commence par cacher les détails
+
+$('.mois2017').hide();
+$('.depenses2017').hide();
+
+$('.mois2016').hide();
+$('.depenses2016').hide();
+
+//Affichage des détails lorsqu'on appuie sur les boutons détails
+
+$('#Bouton2017').click(function(){
+  $('.mois2017').fadeToggle();
+  $('.depenses2017').fadeToggle();
+
+});
+
+$('#Bouton2016').click(function(){
+  $('.mois2016').fadeToggle();
+  $('.depenses2016').fadeToggle();
+
+});
+
+//Consommation 2017
+//On définit les variables à mettre en ordonnées
+var conso_watt_2017_juillet = document.getElementById("conso_watt_2017_juillet").value;
+var conso_watt_2017_aout = document.getElementById("conso_watt_2017_aout").value;
+var conso_watt_2017_septembre = document.getElementById("conso_watt_2017_septembre").value;
+var conso_watt_2017_octobre = document.getElementById("conso_watt_2017_octobre").value;
+var conso_watt_2017_novembre = document.getElementById("conso_watt_2017_novembre").value;
+var conso_watt_2017_decembre = document.getElementById("conso_watt_2017_decembre").value;
+var conso_watt_2017_janvier = document.getElementById("conso_watt_2017_janvier").value;
+var conso_watt_2017_fevrier = document.getElementById("conso_watt_2017_fevrier").value;
+var conso_watt_2017_mars = document.getElementById("conso_watt_2017_mars").value;
+var conso_watt_2017_avril = document.getElementById("conso_watt_2017_avril").value;
+var conso_watt_2017_mai = document.getElementById("conso_watt_2017_mai").value;
+var conso_watt_2017_juin = document.getElementById("conso_watt_2017_juin").value;
+
+
+
+var data = [
+      { y: 'Juillet', a: conso_watt_2017_juillet, b: 90},
+      { y: 'Aout', a: conso_watt_2017_aout ,  b: 75},
+      { y: 'Septembre', a: conso_watt_2017_septembre,  b: 50},
+      { y: 'Octobre', a: conso_watt_2017_octobre,  b: 60},
+      { y: 'Novembre', a: conso_watt_2017_novembre,  b: 65},
+      { y: 'Décembre', a: conso_watt_2017_decembre,  b: 70},
+      { y: 'Janvier', a: conso_watt_2017_janvier, b: 75},
+      { y: 'Février', a: conso_watt_2017_fevrier, b: 75},
+      { y: 'Mars', a: conso_watt_2017_mars, b: 85},
+      { y: 'Avril', a: conso_watt_2017_avril, b: 85},
+      { y: 'Mai', a: conso_watt_2017_mai, b: 95},
+      { y: 'Juin', a: conso_watt_2017_juin, b: 95},
+    ],
+    //On précise les configurations de notre bar chart
+    config = {
+      data: data,
+      xkey: 'y',
+      ykeys: ['a'],
+      labels: ['Consommation',],
+      fillOpacity: 0.6,
+      hideHover: 'auto',
+      barSizeRatio:0.5,
+      behaveLikeLine: true,
+      resize: true,
+      pointFillColors:['#ffffff'],
+      pointStrokeColors: ['black'],
+      lineColors:['gray','red']
+  };
+config.element = 'bar-chart';
+Morris.Bar(config);
+
+
+//Chauffage électrique
+
+var is_elec_heating = document.getElementById("is_elec_heating").value;
+
+
+$('#Vrai').hide();
+$('#Faux').hide();
+
+function AffichageChauffage(){
+    if (is_elec_heating == 'True') {
+        $('#Vrai').show();
+    }
+    else{
+
+        $('#Faux').show();
+    }
+};
+
+AffichageChauffage();
+
+//Dysfonctionnement
+var dysfunction_detected = document.getElementById("dysfunction_detected").value;
+$('.Dysfonctionnement').hide();
+
+//On affiche le message de dysfonctionnement uniquement lorsqu'on en détecte un.
+function AffichageAlerte(){
+    if (dysfunction_detected == 'True'){
+        $('.Dysfonctionnement').show();
+    }
+
+}
+AffichageAlerte();
--- dashboard/views.py
+++ dashboard/views.py
@@ -23,8 +23,6 @@ def results(request, client_id):
     is_elec_heating = True
     dysfunction_detected = False
 
-    print(client_id)
-    print(type(client_id))
 
     ###################################
     # ----> YOUR CODE GOES HERE <---- #
@@ -50,12 +48,63 @@ def results(request, client_id):
     del conso_euro_2017[len(conso_euro_2017)-1]
     #On va sommer les consommations pour obtenir les dépenses annuelles. on arrondit le résultat à 2 décimales
     annual_costs=[round(sum(conso_euro_2016),2),round(sum(conso_euro_2017),2)]
-    print(annual_costs)
 
 
     cursor.execute("SELECT * FROM dashboard_conso_watt WHERE client_id = ?",(client_id,))
     conso_watt = cursor.fetchall()
+    conso_watt_2017 = conso_watt[1]
+    conso_watt_2017 = list(conso_watt_2017)
+    del conso_watt_2017[0]
+    del conso_watt_2017[len(conso_watt_2017)-1]
+    del conso_watt_2017[len(conso_watt_2017)-1]
 
+    #Chauffage électrique
+    #Pour déterminer si l'utilsateur possède un chauffage électrique, on va regarder sa consommation en hiver.
+    #On regarde si parmi les mois de Novembre,Décembre,Janvier,Février (Hiver), 3 de ces mois correspondent
+    #Aux consommations les plus élevés de l'année.
+
+    #Il est tout à fait possible de redéfinir le critère permettant de savoir si l'utilisateur possède un chauffage électrique ou non
+
+    #On travaille sur l'année la plus récente, à savoir 2017.
+
+    Novembre = conso_watt_2017[4]
+    Decembre = conso_watt_2017[5]
+    Janvier = conso_watt_2017[6]
+    Fevrier = conso_watt_2017[7]
+    i=0
+
+    conso_watt_2017_copy = list(conso_watt_2017)
+    del conso_watt_2017_copy[4]
+    del conso_watt_2017_copy[4]
+    del conso_watt_2017_copy[4]
+    del conso_watt_2017_copy[4]
+
+    if Novembre >= max(conso_watt_2017_copy):
+        i=i+1
+    if Decembre >= max(conso_watt_2017_copy):
+        i=i+1
+    if Janvier >= max(conso_watt_2017_copy):
+        i=i+1
+    if Fevrier >= max(conso_watt_2017_copy):
+        i=i+1
+    if i<3:
+        is_elec_heating = False
+
+
+    #Dysfonctionnement
+    #Pour déterminer un dysfonctionnement, on regarde si la consomation entre 2016 n'a pas trop varié.
+    #On va fixer un seuil de 25% pour avoir un critère de dysfonctionnement.
+    #Cela est valable lors d'une hausse, comme d'une baisse.
+
+    a=min(annual_costs)
+    pourcentage=abs(annual_costs[0]-annual_costs[1])
+    print(pourcentage)
+    pourcentage=pourcentage/float(a)
+    pourcentage=pourcentage*100
+    pourcentage=round(pourcentage,2)
+
+    if pourcentage >= 25:
+        dysfunction_detected = True
 
 
 
@@ -93,10 +142,27 @@ def results(request, client_id):
         "conso_euro_2017_juin": round(conso_euro_2017[11],2),
 
         "conso_watt": conso_watt,
+        "conso_watt_2017":conso_watt_2017,
+        "conso_watt_2017_juillet": conso_watt_2017[0],
+        "conso_watt_2017_aout": conso_watt_2017[1],
+        "conso_watt_2017_septembre": conso_watt_2017[2],
+        "conso_watt_2017_octobre": conso_watt_2017[3],
+        "conso_watt_2017_novembre": conso_watt_2017[4],
+        "conso_watt_2017_decembre": conso_watt_2017[5],
+        "conso_watt_2017_janvier": conso_watt_2017[6],
+        "conso_watt_2017_fevrier": conso_watt_2017[7],
+        "conso_watt_2017_mars": conso_watt_2017[8],
+        "conso_watt_2017_avril": conso_watt_2017[9],
+        "conso_watt_2017_mai": conso_watt_2017[10],
+        "conso_watt_2017_juin": conso_watt_2017[11],
+
         "annual_costs": annual_costs,
         "Depense_2016": annual_costs[0],
         "Depense_2017": annual_costs[1],
+
         "is_elec_heating": is_elec_heating,
-        "dysfunction_detected": dysfunction_detected
+
+        "dysfunction_detected": dysfunction_detected,
+        "pourcentage":pourcentage,
     }
     return render(request, 'dashboard/results.html', context)
